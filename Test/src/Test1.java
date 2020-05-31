a782ea5bc31af2019a0a2a9f2446dc85d9e14c61
jooy3410 <jooy3410@naver.com>
2020-06-01 00:08:57 +0900

    곱셈

---
+++ Test/src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int i,k;
+		
+		for(i=2;i<=8;i++) {
+			if(i==3 || i==5) {
+			System.out.println("<"+i+"´Ü>");
+			}
+			for(k=1;k<=9;k++) {
+				System.out.printf("%d*%d=%d\n", i,k,(i*k));
+				}
+			System.out.printf("\n");
+			
+		}
+
+	}
+
+}
