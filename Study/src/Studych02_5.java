cf223408aa7cb8a6382699f80696d89bc89a093e
jooy3410 <jooy3410@naver.com>
2020-06-07 11:10:16 +0900

    .

---
+++ Study/src/Studych02_5.java
@@ -0,0 +1,15 @@
+
+public class Studych02_5 {
+
+	public static void main(String[] args) {
+		final int score = 100;//final을 입력해주어 score을 상수로 변화해줘서
+		// 다른것으로 바꿔줄수가 없다.
+//		score = 200;
+//		변수의 선언과 초기화를 따로해줄수 있지만
+//		final int score = 100;처럼 선언과 초기화를 동시에 해주는것이 좋다
+		System.out.println(score);
+		
+
+	}
+
+}
