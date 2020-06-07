1a674efe2b1ce01f685e5dd27813b8e31475e67e
jooy3410 <jooy3410@naver.com>
2020-06-07 10:11:16 +0900

    1008번

---
+++ Baekjoon/src/Main1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main1008 {
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		double a = s.nextDouble();
+		double b = s.nextDouble();
+		
+		System.out.println(a/b);
+
+	}
+
+}
