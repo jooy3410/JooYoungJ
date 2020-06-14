69e08607df45f2dde44ac54cc7b62553d45bc1ea
jooy3410 <jooy3410@naver.com>
2020-06-14 23:42:18 +0900

    10950

---
+++ Baekjoon/src/Main10950.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main10950 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int len = sc.nextInt();
+		
+		for(int i = 0; i < len; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+
+
+	}
+
+
