55535fe4e305d1784d85c76a02dcbe46dcb8143c
jooy3410 <jooy3410@naver.com>
2020-06-15 23:07:35 +0900

    자바의 정석 ch2-12

---
+++ Study/src/Studych2__12.java
@@ -0,0 +1,19 @@
+
+public class Studych2__12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10.0/3);
+		
+		System.out.printf("%d%n", 15);
+		System.out.printf("%#o%n",  15);
+		System.out.printf("%#x%n",  15);
+		System.out.printf("%s",Integer.toBinaryString(15));
+		
+		double f = 123.456789;
+		System.out.printf("%f$n", f);
+		System.out.printf("%e%n", f);
+		System.out.printf("%g%n",  f);
+	}
+
+}
