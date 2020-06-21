8c284464065b5b73b1b6a1a183c40317979891dd
jooy3410 <jooy3410@naver.com>
2020-06-22 01:02:02 +0900

    배열 연습

---
+++ Test/src/Test10.java
@@ -0,0 +1,23 @@
+
+public class Test10 {
+
+	public static void main(String[] args) {
+		int [] num= new int[90];
+		int k=11;
+		int hap=0;
+		
+		for(int i=0; i<90;i++) {
+			num[i]=k;
+			hap +=num[i];
+			k++;
+		}
+		int i=0;
+		
+		while(i<90) {
+			System.out.printf("num[%d] = %d\n", i,num[i]);
+			i++;
+		}
+		System.out.println("гу : " + hap);
+	}
+
+}
