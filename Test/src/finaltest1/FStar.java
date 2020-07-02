da228f687e376b906142ac6ffb44b5de2bf2c1d7
jooy3410 <jooy3410@naver.com>
2020-07-03 01:31:23 +0900

    .

---
+++ Test/src/finaltest1/FStar.java
@@ -0,0 +1,26 @@
+package finaltest1;
+
+public class FStar {
+
+	public static void main(String[] args) {
+int j=4;
+		
+		for(int i=1; i<=7; i++) {
+			for(int k=1; k<=4; k++) {
+				if (k < j) {
+					System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			if (i < 4) {
+				j--;
+			}else {
+				j++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
