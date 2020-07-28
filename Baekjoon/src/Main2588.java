5988d261c0261eae68d50399c1fde4627ac7d65b
jooy3410 <jooy3410@naver.com>
2020-07-28 14:08:53 +0900

    2588번

--- Baekjoon/src/Main2588.java
+++ Baekjoon/src/Main2588.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class Main2588 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		int k = sc.nextInt();
+		
+		System.out.println(i * (k % 10));
+		System.out.println(i * ((k % 100) / 10));
+		System.out.println(i * (k / 100));
+		System.out.println(i * k);
 	}
 
 }
