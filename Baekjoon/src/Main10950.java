1dadcd8adbe6c67439cf6c44a3c86953e5b0f4bb
jooy3410 <jooy3410@naver.com>
2020-09-10 22:37:08 +0900

    백준10950번

--- Baekjoon/src/Main10950.java
+++ Baekjoon/src/Main10950.java
@@ -5,9 +5,9 @@ public class Main10950 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int len = sc.nextInt();
+		int k = sc.nextInt();
 		
-		for(int i = 0; i < len; i++) {
+		for(int i = 0; i < k; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
 			System.out.println(a+b);
