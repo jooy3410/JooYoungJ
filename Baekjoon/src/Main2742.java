da64ef84300647ae3eff47fcac3b6528b0a2582b
jooy3410 <jooy3410@naver.com>
2020-09-17 19:24:36 +0900

    백준2742번

---
+++ Baekjoon/src/Main2742.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main2742 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		
+		int n =sc.nextInt();
+		sc.close();
+		
+		for(int i =n ; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
